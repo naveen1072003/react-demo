c90f563b8b37eba3f56c1f815469c1c1d8d5f6f7
naveen1072003 <naveenrangaraju450@gmail.com>
2023-09-14 14:50:01 +0530

    second commit

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,31 +1,99 @@
 import { useEffect, useState } from "react";
-import axios from "axios"
-import { deleteUser_Url, getAllUsers_Url } from "./constraints/apiUrl";
+import "./Table.css";
+import axios from "axios";
+import {
+  addUser_Url,
+  deleteUser_Url,
+  editUser_Url,
+  getAllUsers_Url,
+} from "./constraints/apiUrl";
 
+import logo from "../components/images/Divum_Logo.svg";
 function Table() {
   const [userList, setUsers] = useState("");
 
-//   const axios = require("axios")
+  const [addFormDetails, setaddFormDetails] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    phNo: "",
+    address: "",
+    dob: "",
+  });
 
-  // get All Users
+  const [editDetails, setEditDetails] = useState({});
+
+  console.log(addFormDetails, "=====================");
+  console.log(editDetails, "??????????????????");
+  const [isFormVisible, setFormVisibile] = useState(false);
+  const [isEditForm, setEditForm] = useState(false);
+
+  const resetForm = () => {
+    setEditDetails({});
+    setEditForm(false);
+    setFormVisibile(false);
+  };
+
+  // const changeEditForm = () => {
+  //     setEditForm(true)
+  // };
+
+  const OnChange = (e) => {
+    if (editDetails.email === "") {
+      setaddFormDetails((addFormDetails) => ({
+        ...addFormDetails,
+        [e.target.name]: e.target.value,
+      }));
+    } else {
+      setEditDetails((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value,
+      }));
+    }
+  };
+
+  // Add User Api Call ====
+
+  const add_EditApi = async () => {
+    try {
+      if (editDetails.email === "") {
+        const addresponse = await axios.post(addUser_Url, {
+          ...addFormDetails,
+        });
+        console.log(addresponse);
+      } else {
+        const putresponse = await axios.put(editUser_Url, {
+          ...editDetails,
+        });
+      }
+      setFormVisibile(false);
+    } catch (err) {
+      console.log(err);
+    }
+
+    setEditDetails({});
+  };
+
+  // get All Users Call ======
 
   const callApi = async () => {
     try {
-        console.log(1);
-    //   const objUsers = await axios.get("http://localhost:8080/user/getAllUsers");
-    const objUsers = await axios.get("http://localhost:8080/user/getAllUsers");
-    
-        setUsers(objUsers.data);
+      console.log(1);
+      const objUsers = await axios.get(getAllUsers_Url);
+
+      setUsers(objUsers.data);
     } catch (e) {
       console.log(e);
     }
   };
   useEffect(() => {
     callApi();
-  }, []);
-  console.log(userList,"hvjj");
+  }, [isFormVisible]);
 
-  // delete User
+  console.log(editDetails, "Edit Details");
+
+  // Delete User Api Call ======
 
   const deleteApi = async () => {
     try {
@@ -34,152 +102,157 @@ function Table() {
       console.log(e);
     }
   };
-//  if(!userList)
-//  return "loading"
+
+  console.log(isEditForm, "edit form");
 
   return (
-    <>
-       <div className="head">
-        <img
-          src="/images/Divum_Logo.svg"
-          alt=""
-          width="90"
-          height="100"
-        />
+    <div className="form-wrapper">
+      <div className="head">
+        <img src={logo} alt="" width="90" height="100" />
         <h1>User Details</h1>
       </div>
       <div className="add">
-        <button id="addBtn" onClick="showTemp()">
+        <button type="reset" id="addBtn" onClick={() => setFormVisibile(true)}>
           Add User
         </button>
       </div>
-     {/* <p th:text="${result}" style="display: none" id="result"></p>  */}
-      <div className="form-container">
-        <div className="form">
-          <form id="editUserForm">
-            <div className="form-header">
-              <h1>Add User</h1>
-              <button type="reset" className="btnrst" 
-            //   onClick={reset1()}
-              >
-                <i className="fas fa-times"></i>
-              </button>
-            </div>
-            <label htmlFor="first_name">
-              First name<span> *</span>
-            </label>
-            <br />
-            <input
-              type="text"
-              id="first_name"
-              data-testid="nametest"
-            //   onBlur={checkFname(this)}
-              name="firstName"
-              required
-            />{" "}
-            <br />
-            <p id="fname"></p>
-            <label htmlFor="last_name">
-              Last name <span > *</span>
-            </label>
-            <br />
-            <input
-              type="text"
-              id="last_name"
-              data-testid="lnametest"
-            //   onBlur={checkLname(this)}
-              name="lastName"
-              required
-            />
-            <br />
-            <p id="lname"></p>
-            <label htmlFor="email">
-              Email <span > *</span>
-            </label>
-            <br />
-            <input
-              type="email"
-              id="email"
-            //   onBlur={errorEmail(this)}
-              name="email"
-              required
-            />
-            <br />
-            <p id="emailerror"></p>
-            <div id="passId">
-              <label htmlFor="pass">
-                Password<span > *</span>
-                <i className="far fa-eye" id="togglePassword"></i>
+      {console.log(isFormVisible)}
+      {isFormVisible && (
+        <div className="form-container">
+          <div className="form">
+            <form id="editUserForm">
+              <div className="form-header">
+                <h1>{editDetails.email === "" ? "Add User" : "Edit user"} </h1>
+
+                <button type="reset" className="btnrst" onClick={resetForm}>
+                  <i className="fas fa-times"></i>
+                </button>
+              </div>
+              <label htmlFor="first_name">
+                First name<span> *</span>
               </label>
               <br />
               <input
-                type="password"
-                id="pass"
-                data-testid=""
-                // onBlur={verifyPassword(this)}
-                name="password"
-                maxLength="15"
+                type="text"
+                id="first_name"
+                data-testid="nametest"
+                value={editDetails.firstName}
+                onChange={OnChange}
+                name="firstName"
                 required
               />
               <br />
-              <p id="pwd"></p>
-            </div>
-            <label htmlFor="pno">
-              Phone No<span > *</span>
-            </label>
-            <br />
-            <input
-              type="text"
-              id="pno"
-              data-testid="phnotest"
-            //   onBlur={checkPhno(this)}
-              name="phNo"
-              maxLength="10"
-              required
-            />
-            <br />
-            <p id="phno"></p>
-            <label htmlFor="address">
-              Address<span > *</span>
-            </label>
-            <br />
-            <textarea
-              id="address"
-              data-testid="addresstest"
-            //   onBlur={checkAdd(this)}
-              name="Address"
-              maxLength="50"
-              required
-            ></textarea>
-            <br />
-            <p id="add">
-              <i className="fa fa-warning" ></i> Maximum 50
-              characters!
-            </p>
-            <label htmlFor="dateOB">Date Of Birth</label>
-            <br />
-            <input
-              type="date"
-              id="dateOB"
-              data-testid="datetest"
-              name="dob"
-              required
-            />
-            <br />
-            <br />
-            <div className="sbtn">
-              <button
-                type="button"
-                id="form-button"
-                data-testid="submitBtn"
-                // onClick={handleSubmit()}
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+              <p id="fname"></p>
+              <label htmlFor="last_name">
+                Last name <span> *</span>
+              </label>
+              <br />
+              <input
+                type="text"
+                id="last_name"
+                data-testid="lnametest"
+                value={editDetails.lastName}
+                onChange={OnChange}
+                name="lastName"
+                required
+              />
+              <br />
+              <p id="lname"></p>
+              <label htmlFor="email">
+                Email <span> *</span>
+              </label>
+              <br />
+              <input
+                type="email"
+                id="email"
+                onChange={OnChange}
+                value={editDetails.email}
+                name="email"
+                required
+              />
+              <br />
+              <p id="emailerror"></p>
+
+              {!isEditForm && (
+                <div id="passId">
+                  <label htmlFor="pass">
+                    Password<span> *</span>
+                    <i className="far fa-eye" id="togglePassword"></i>
+                  </label>
+                  <br />
+                  <input
+                    type="password"
+                    id="pass"
+                    data-testid=""
+                    onChange={OnChange}
+                    value={editDetails.password}
+                    name="password"
+                    maxLength="15"
+                    required
+                  />
+                  <br />
+                  <p id="pwd"></p>
+                </div>
+              )}
+              <label htmlFor="pno">
+                Phone No<span> *</span>
+              </label>
+              <br />
+              <input
+                type="text"
+                id="pno"
+                data-testid="phnotest"
+                onChange={OnChange}
+                value={editDetails.phNo}
+                name="phNo"
+                maxLength="10"
+                required
+              />
+              <br />
+              <p id="phno"></p>
+              <label htmlFor="address">
+                Address<span> *</span>
+              </label>
+              <br />
+              <textarea
+                id="address"
+                data-testid="addresstest"
+                onChange={OnChange}
+                value={editDetails.address}
+                name="address"
+                maxLength="50"
+                required
+              ></textarea>
+              <br />
+              <p id="add">
+                <i className="fa fa-warning"></i> Maximum 50 characters!
+              </p>
+              <label htmlFor="dateOB">Date Of Birth</label>
+              <br />
+              <input
+                type="date"
+                id="dateOB"
+                onChange={OnChange}
+                value={editDetails.dob}
+                name="dob"
+                required
+              />
+              <br />
+              <br />
+              <div className="sbtn">
+                <button
+                  type="button"
+                  id="form-button"
+                  data-testid="submitBtn"
+                  onClick={add_EditApi}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
       <div className="thead">
         <table>
           <thead>
@@ -192,30 +265,32 @@ function Table() {
             <th>Actions</th>
           </thead>
           <tbody>
-            
-              {/* {console.log(userList.content,"singam")} */}
-            {!userList && userList.content.map((user) => {
-              return (
-                <tr>
-                  <td>{user.firstName}</td>
-                  <td>{user.lastName}</td>
-                  <td>{user.email}</td>
-                  <td>{user.dob}</td>
-                  <td>{user.phNo}</td>
-                  <td>{user.address}</td>
-                  <td className="action-buttons">
-                    <button
-                      className="edit"
-                      value={user.id}
-                    >
-                      <i
-                        className="material-icons"
-                        data-toggle="tooltip"
-                        title="Edit"
-                      >
-                        &#xE254;
-                      </i>
-                    </button>
+            {/* {console.log(userList.content,"singam")} */}
+            {userList &&
+              userList.content.map((user) => {
+                return (
+                  <tr>
+                    <td>{user.firstName}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.email}</td>
+                    <td>{user.dob}</td>
+                    <td>{user.phNo}</td>
+                    <td>{user.address}</td>
+                    <td className="action-buttons">
+                      <button className="edit" value={user.id}>
+                        <i
+                          className="material-icons"
+                          data-toggle="tooltip"
+                          title="Edit"
+                          onClick={() => {
+                            setEditForm(true);
+                            setEditDetails(user);
+                            setFormVisibile(true);
+                          }}
+                        >
+                          &#xE254;
+                        </i>
+                      </button>
                       <button
                         className="delete"
                         onClick={() => deleteApi(user.id)}
@@ -229,14 +304,14 @@ function Table() {
                           &#xE872;
                         </i>
                       </button>
-                  </td>
-                </tr>
-              );
-            })}
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
-      </div> 
-    </>
+      </div>
+    </div>
   );
 }
 
--- src/components/constraints/apiUrl.js
+++ src/components/constraints/apiUrl.js
@@ -1,2 +1,4 @@
 export const getAllUsers_Url = "http://localhost:8080/user/getAllUsers"
 export const deleteUser_Url = "http://localhost:8080/user/deleteUser/"
+export const addUser_Url = "http://localhost:8080/user/newUser"
+export const editUser_Url = "http://localhost:8080/user/updateUser"
